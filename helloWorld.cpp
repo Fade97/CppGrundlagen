635e904aed367b6b850072cd174c08e4957060bd
Fabian <mail@fabian-duerkop.de>
2022-02-02 09:55:54 +0100

    Initial

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main( int argc, char** args )
+{
+	string a = "Hallo Welt";
+	cout << a << "!" << endl;
+	return 0;
+}
